236325527b417085bb2109b9210d5cd750ba595b
FrmJ <70322094+FrmJ@users.noreply.github.com>
2026-05-06 11:54:45 -0700

    add sketch board generator

---
+++ script.js
@@ -0,0 +1,12 @@
+const generateBoard = (grid) => {
+    const board = document.querySelector("div.board-container");
+    for (let i = 0 ; i < grid; i++) {
+        const box = document.createElement("div");
+        box.classList = `grid-${i}`;
+        box.style.height = "10px";
+        box.style.width = "10px";
+        board.append(box);
+    }
+};
+
+generateBoard(12);
